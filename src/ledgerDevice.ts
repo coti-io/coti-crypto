65a55d0c3b45e5cbb396ac318794d9f2eaf17ea8
cotitech <eli@coti.io>
2020-12-20 16:25:09 +0200

    ledger log

--- src/ledgerDevice.ts
+++ src/ledgerDevice.ts
@@ -1,5 +1,7 @@
 import { EventEmitter } from 'events';
 import { Descriptor, DescriptorEvent } from '@ledgerhq/hw-transport';
+//import type Log from '@ledgerhq/logs';
+import { listen } from '@ledgerhq/logs';
 import * as ledgerUtils from './utils/ledgerUtils';
 
 type LedgerTransportType = ledgerUtils.LedgerTransportType;
@@ -7,9 +9,11 @@ type LedgerTransportType = ledgerUtils.LedgerTransportType;
 export interface LedgerEvent {
   on(event: 'add' | 'remove', listener: (ledgerEvent: DescriptorEvent<Descriptor>) => void): this;
   on(event: 'error', listener: (error: Error) => void): this;
+  on(event: 'log', listener: (ledgerLog: any) => void): this;
 
   emit(event: 'add' | 'remove', ledgerEvent: DescriptorEvent<Descriptor>): boolean;
   emit(event: 'error', error: Error): boolean;
+  emit(event: 'log', ledgerLog: any): boolean;
 }
 
 export abstract class LedgerEvent extends EventEmitter {
@@ -36,6 +40,10 @@ export class LedgerDevice extends LedgerEvent {
 
   public listen() {
     ledgerUtils.listen({ next: event => this.next(event), error: error => this.error(error), complete: () => this.complete() }, this.transportType);
+  }
+
+  public listenLog() {
+    listen((ledgerLog: any) => this.emit('log', ledgerLog));
   }
 
   public next(event: DescriptorEvent<Descriptor>) {
--- src/module.d.ts
+++ src/module.d.ts
@@ -1,1 +1,2 @@
 declare module 'blakejs';
+declare module '@ledgerhq/logs';
